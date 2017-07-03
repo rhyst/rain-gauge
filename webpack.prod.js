f086d810761fe48a3fe469208e101850a472d012
Rhys Tyers <rhystyers1@gmail.com>
2017-07-03 12:50:39 +0100

    Made copy-webpack-plugin replace bundle.js with bundle.min.js in prod build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,22 +1,24 @@
-const Merge = require('webpack-merge');
+const Merge = require("webpack-merge");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const webpack = require("webpack");
-
 
 const config = require("./webpack.dev");
 
-module.exports = Merge(config, {
+module.exports = Merge.strategy({plugins: 'replace'})(config, {
     output: {
         filename: "bundle.min.js"
     },
-    module : {
-        loaders: [{
-            test: /index\.html$/,
-            loader: 'string-replace',
-            query: {
-                search: 'bundle.js',
-                replace: 'bundle.min.js'
+    module: {
+        loaders: [
+            {
+                test: /index\.html$/,
+                loader: "string-replace",
+                query: {
+                    search: "bundle.js",
+                    replace: "bundle.min.js"
+                }
             }
-        }]
+        ]
     },
     plugins: [
         new webpack.optimize.UglifyJsPlugin(),
@@ -24,6 +26,13 @@ module.exports = Merge(config, {
             "process.env": {
                 NODE_ENV: JSON.stringify("production")
             }
-        })
+        }),
+        new CopyWebpackPlugin([
+            { from: "style", to: "style" },
+            { from: "index.html", to: "index.html", transform: function(content, path) {
+                return content.toString().replace('bundle.js', 'bundle.min.js')
+            } },
+            { from: "selectedpin.png", to: "selectedpin.png" }
+        ])
     ]
 });
