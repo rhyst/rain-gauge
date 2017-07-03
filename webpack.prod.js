3b4cb7b08e8c16513c1ff5e137d058a1d4821b84
Rhys Tyers <rhystyers1@gmail.com>
2017-07-03 11:38:50 +0000

    Adding replace plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,22 @@
 const Merge = require('webpack-merge');
 const webpack = require("webpack");
 
+
 const config = require("./webpack.dev");
 
 module.exports = Merge(config, {
     output: {
         filename: "bundle.min.js"
+    },
+    module : {
+        loaders: [{
+            test: /index\.html$/,
+            loader: 'string-replace',
+            query: {
+                search: 'bundle.js',
+                replace: 'bundle.min.js'
+            }
+        }]
     },
     plugins: [
         new webpack.optimize.UglifyJsPlugin(),
